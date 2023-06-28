32e8c7495492435107e10b7a1be4d885e64559a8
Nuria Oujo <nuriaoujo.n1@gmail.com>
2023-06-28 09:40:04 +0200

    añadidas funciones y test de números pares

--- src/app/karma-jasmine.service.ts
+++ src/app/karma-jasmine.service.ts
@@ -21,4 +21,13 @@ export class KarmaJasmineService {
   dividir(a:number, b:number): number {
     return a/b;
   }
+  //Función para verificar si un número es par
+  numeroPar(a:number): string{
+  if(a % 2 == 0){
+    return "Es par"
+  }
+}
+  //Función para verificar si una cadena es un palíndromo
+
+  //Función para calcular el factorial de un número
 }
